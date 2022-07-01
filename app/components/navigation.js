d3aad735321499115dc7d23bd871c38f2858bb83
BlueTompon <lucastran00@gmail.com>
2022-07-01 19:35:03 +0200

    navigation logic

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,4 +1,5 @@
 import Components from "../classes/components";
+import { COLOR_BRIGHT_GREY, COLOR_QUARTER_WHITE } from "../utils/color";
 
 export default class Navigation extends Components {
   constructor({ template }) {
@@ -10,11 +11,25 @@ export default class Navigation extends Components {
       }
     })
 
+    this.navMap = {
+      show: {
+        'collections': 1,
+        'about': 0
+      },
+      hide: {
+        'collections': 0,
+        'about': 1
+      }
+    }
+
     this.onChange(template)
   }
 
 
   onChange(template) {
-    console.log('template', template);
+    console.log('template', template, this.elements.items);
+    this.element.style.color = template == 'about' ? COLOR_BRIGHT_GREY : COLOR_QUARTER_WHITE;
+    this.elements.items[this.navMap.hide[template]].style.visibility = 'hidden'
+    this.elements.items[this.navMap.show[template]].style.visibility = 'visible'
   }
 }
---
+++ app/utils/color.js
@@ -0,0 +1,2 @@
+export const COLOR_QUARTER_WHITE = '#F9F1E7';
+export const COLOR_BRIGHT_GREY = '#37384C';
